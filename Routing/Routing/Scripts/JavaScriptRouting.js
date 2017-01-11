bb838254a4538eefe197d63acdc03fd24be17b78
Abin <Abin@IT6-ABIN>
2017-01-11 17:56:29 +0600

    commit route project to online

---
+++ Routing/Routing/Scripts/JavaScriptRouting.js
@@ -0,0 +1,41 @@
+﻿
+
+var testApp = angular.module('testApp', ['ngRoute']);
+
+testApp.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'Partial/home.html',
+            controller: 'MainController'
+        })
+
+        .when('/about', {
+            templateUrl: 'Partial/about.html',
+            controller: 'AboutController'
+        })
+
+        .when('/contact', {
+            templateUrl: 'Partial/contact.html',
+            controller: 'ContactController'
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        });
+
+    $locationProvider.html5Mode(true);
+});
+
+
+
+testApp.controller('MainController', function ($scope) {
+    $scope.message = 'This our home page, have a fun!';
+});
+
+testApp.controller('AboutController', function ($scope) {
+    $scope.message = 'This our about page, have a fun!';
+});
+
+testApp.controller('ContactController', function ($scope) {
+    $scope.message = 'This our contact page, have a fun!';
+});
